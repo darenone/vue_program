adc10528d685e28488a297efee05431d02a1cbd0
Zongq <1291342008@qq.com>
2017-12-24 14:09:45 +0800

    搭建项目

---
+++ src/components/slideshow/index1.vue
@@ -0,0 +1,156 @@
+<template>
+  <div class="slide-show" @mouseover="clearInv" @mouseout="runInv">
+    <!-- jianjie -->
+    <div class="slide-title">
+      <p style="padding-top:30px;">
+        <B>自定义组件__幻灯片</B><br/>
+        可一个给幻灯片传递 slides inv两个值
+      </p>
+    </div>
+    <!-- 图片 -->
+    <div class="slide-img">
+      <a :href="slides[nowIndex].href">
+        <transition name="slide-trans">
+          <img v-if="isShow" :src="slides[nowIndex].src">
+        </transition>
+        <transition name="slide-trans-old">
+          <img v-if="!isShow" :src="slides[nowIndex].src">
+        </transition>
+      </a>
+    </div>
+    <!-- 标题 -->
+    <h2>{{ slides[nowIndex].title }}</h2>
+    <!-- 页码 -->
+    <ul class="slide-pages">
+      <li @click="goto(prevIndex)">&lt;</li>
+      <li v-for="(item, index) in slides" @click="goto(index)">
+        <a :class="{on: index === nowIndex}">{{ index + 1 }}</a>
+      </li>
+      <li @click="goto(nextIndex)">&gt;</li>
+    </ul>
+    <p>{{ prevIndex }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    slides: {
+      type: Array,
+      default: []
+    },
+    inv: {
+      type: Number,
+      default: 1000
+    }
+  },
+  data () {
+    return {
+      nowIndex: 0,
+      isShow: true
+    }
+  },
+  computed: {
+    prevIndex () {
+      if (this.nowIndex === 0) {
+        return this.slides.length - 1
+      } else {
+        return this.nowIndex - 1
+      }
+    },
+    nextIndex () {
+      if (this.nowIndex === this.slides.length - 1) {
+        return 0
+      } else {
+        return this.nowIndex + 1
+      }
+    }
+  },
+  methods: {
+    goto (index) {
+      this.isShow = false
+      setTimeout(() => {
+        this.isShow = true
+        this.nowIndex = index
+        // this.$emit('onchange', index)
+      }, 10)
+    },
+    runInv () {
+      this.invId = setInterval(() => {
+        this.goto(this.nextIndex)
+      }, this.inv)
+    },
+    clearInv () {
+      clearInterval(this.invId)
+    }
+  },
+  mounted () {
+    console.log(this.slides)
+    this.runInv()
+  }
+}
+</script>
+
+<style scoped>
+.slide-trans-enter-active {
+  transition: all .5s;
+}
+.slide-trans-enter {
+  transform: translateX(900px);
+}
+.slide-trans-old-leave-active {
+  transition: all .5s;
+  transform: translateX(-900px);
+}
+.slide-show {
+  position: relative;
+  margin: 15px 15px 15px 0;
+  width: 900px;
+  height: 500px;
+  overflow: hidden;
+}
+.slide-show .slide-title {
+  position: absolute;
+  z-index: 1000;
+  width: 300px;
+  height: 300px;
+  background-color: rgba(176, 160, 160, 0.42);
+  border-radius: 20px;
+  left: 30px;
+  color: white;
+}
+.slide-show h2 {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  color: #fff;
+  background: #000;
+  opacity: .5;
+  bottom: 0;
+  height: 30px;
+  text-align: left;
+  padding-left: 15px;
+}
+.slide-img {
+  width: 100%;
+}
+.slide-img img {
+  width: 100%;
+  position: absolute;
+  top: 0;
+}
+.slide-pages {
+  position: absolute;
+  bottom: 10px;
+  right: 15px;
+}
+.slide-pages li {
+  display: inline-block;
+  padding: 0 10px;
+  cursor: pointer;
+  color: #fff;
+}
+.slide-pages li .on {
+  text-decoration: underline;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import homepage from '../view/homepage/index.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '',
+      name: 'homepage',
+      component: homepage
     }
   ]
 })
